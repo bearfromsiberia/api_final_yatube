54ac924bbe94251899c70acbc6c786656ef11213
Ilya <lomteb38@gmail.com>
2025-02-04 13:36:03 +0700

    ex

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,48 +3,49 @@ from posts.models import Comment, Post, Follow, Group
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.CharField(source='author.username', read_only=True)
+    author = serializers.CharField(source="author.username", read_only=True)
     group = serializers.PrimaryKeyRelatedField(
-        queryset=Group.objects.all(), allow_null=True, required=False
+        queryset=Group.objects.all(),
+        allow_null=True,
+        required=False,
     )
 
     class Meta:
         model = Post
-        fields = ['id', 'author', 'text', 'pub_date', 'image', 'group']
-        read_only_fields = ['id', 'pub_date', 'author']
-
+        fields = ["id", "author", "text", "pub_date", "image", "group"]
+        read_only_fields = ["id", "pub_date", "author"]
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
+    author = serializers.SlugRelatedField(read_only=True, slug_field="username")
 
     class Meta:
-        fields = ['id', 'author', 'post', 'text', 'created']
-        read_only_fields = ['author', 'post']
         model = Comment
+        fields = ["id", "author", "post", "text", "created"]
+        read_only_fields = ["author", "post"]
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ['id', 'title', 'slug', 'description']
+        fields = ["id", "title", "slug", "description"]
 
 
 class FollowSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(source='user.username',
-                                read_only=True)
+    user = serializers.CharField(source="user.username", read_only=True)
     following = serializers.CharField(
-                                    source='following.username',
-                                    read_only=True
-                                    )
+        source="following.username",
+        read_only=True,
+    )
 
     class Meta:
         model = Follow
-        fields = ['user', 'following']
-        # Добавляем валидатор для уникальности пары (user, following)
+        fields = ["user", "following"]
 
     def validate_following(self, value):
-        # Проверяем, что пользователь не может подписаться на самого себя
-        if self.context['request'].user == value:
+        """
+        Проверяем, что пользователь не может подписаться на самого себя.
+        """
+        if self.context["request"].user == value:
             raise serializers.ValidationError("Вы не можете подписаться на себя.")
         return value
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -8,37 +8,38 @@ class Group(models.Model):
     """
     Модель для представления группы.
     Хранит информацию о названии группы,
-    ее уникальном идентификаторе и описании
+    ее уникальном идентификаторе и описании.
     """
-    # Название группы, максимальная длина 200 символов
-    title = models.CharField(max_length=200)
-    # Уникальный идентификатор для группы, используется в URL
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=200)  # Название группы
+    slug = models.SlugField(unique=True)  # Уникальный идентификатор для группы
     description = models.TextField()  # Описание группы
 
     def __str__(self):
         """
         Возвращает строковое представление объекта Group.
-        Используем для отображения названия
-        группы в админке и др. местах
+        Используется для отображения названия группы в админке.
         """
-        return self.title  # Возвращаем название группы
+        return self.title
 
 
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(User,
-                                on_delete=models.CASCADE,
-                                related_name='posts'
-                                )
-    image = models.ImageField(upload_to='posts/', null=True, blank=True)
-    group = models.ForeignKey(Group,
-                               on_delete=models.SET_NULL,
-                                blank=True, null=True)
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    image = models.ImageField(upload_to="posts/", null=True, blank=True)
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
     class Meta:
-        ordering = ('-pub_date',)  # Сортировка по дате публикации по убыванию
+        ordering = ("-pub_date",)  # Сортировка по дате публикации по убыванию
 
     def __str__(self):
         return self.text
@@ -46,40 +47,44 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
     text = models.TextField()
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        "Дата добавления",
+        auto_now_add=True,
+        db_index=True,
+    )
 
 
 class Follow(models.Model):
     """
     Модель для представления подписок пользователей.
-    Хранит информацию о том, кто подписан на кого подписан
+    Хранит информацию о том, кто на кого подписан.
     """
     user = models.ForeignKey(
         User,
-        related_name='following',
-        # Удаляем подписки, если пользователь удален
-        on_delete=models.CASCADE
+        related_name="following",
+        on_delete=models.CASCADE,  # Удаляем подписки, если пользователь удален
     )
     following = models.ForeignKey(
         User,
-        # Имя обратной связи для получения подписчиков текущего пользователя
-        related_name='followers',
-        # Удаляем подписки, если пользователь удален
-        on_delete=models.CASCADE
+        related_name="followers",  # Имя обратной связи для получения подписчиков
+        on_delete=models.CASCADE,  # Удаляем подписки, если пользователь удален
     )
 
     class Meta:
-        # Обеспечиваем уникальность пары (пользователь, на которого подписан)
-        unique_together = ('user', 'following')
+        unique_together = ("user", "following")  # Уникальная пара (user, following)
 
     def __str__(self):
         """
         Возвращает строковое представление объекта Follow.
         """
-        # Форматируем строку для удобства чтения
         return f"{self.user.username} follows {self.following.username}"
