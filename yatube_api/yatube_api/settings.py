888ce1a18832d1409274fcbe8d244fdbdd3e1259
Ilya <lomteb38@gmail.com>
2025-02-04 12:17:13 +0700

    ex

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,11 +1,7 @@
 from pathlib import Path
 
 from datetime import timedelta
-import environ
 
-env = environ.Env()
-
-environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
